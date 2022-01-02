ac6dc5afab492b059b4493cf08601506538027c0
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-01-02 17:11:22 -0300

    fix envoriment

--- src/client/src/environments/environment.prod.ts
+++ src/client/src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment = {
   OWS_O2: "https://o2.lapig.iesa.ufg.br/ows",
   OWS_O3: "https://o3.lapig.iesa.ufg.br/ows",
   OWS_O4: "https://o4.lapig.iesa.ufg.br/ows",
-  DEFAULT_LAYERS: ['pasture']
+  ATLAS_URL: 'https://atlas-homolog.lapig.iesa.ufg.br'
 };
