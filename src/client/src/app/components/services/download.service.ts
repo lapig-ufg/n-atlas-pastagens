226977c84f7e16a37f0b3c4436871104b1b355ee
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-01-04 12:04:48 -0300

    update hotsite

--- src/client/src/app/components/services/download.service.ts
+++ src/client/src/app/components/services/download.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import {environment} from "../../../environments/environment";
+import { environment } from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DownloadService {
 
-  private apiURL = '/service/download';
+  private apiURL = `${environment.OWS}/api/download`;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   downloadRequest(parameters): Observable<Blob> {
     return this.httpClient.post(this.apiURL, parameters, { responseType: 'blob' })
   }
+
   downloadRequestSLD(layer): Observable<Blob> {
     const url = `${environment.OWS}/ows?request=GetStyles&layers=${layer}&service=wms&version=1.1.1`;
     return this.httpClient.get(url, { responseType: 'blob' })
